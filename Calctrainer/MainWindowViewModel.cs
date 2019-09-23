b2bcb2cf25caba44d9e2ad91ea4267226ca987bd
dja <djawna@gmail.com>
2019-09-23 12:10:50 +0200

    add first commit

---
+++ Calctrainer/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calctrainer
+{
+    public class MainWindowViewModel : DependencyObject
+    {
+        #region DependencyPropertyStaticBoylerplateCode
+
+        public static readonly DependencyProperty ProfileNameProperty =
+            DependencyProperty.Register("ProfileNameProperty", typeof(string), typeof(MainWindowViewModel), new PropertyMetadata(""));
+
+
+        #endregion DependencyPropertyStaticBoylerplateCode
+
+
+        public string ProfileName
+        {
+            get { return (string)GetValue(ProfileNameProperty); }
+            set { SetValue(ProfileNameProperty, value); }
+        }
+
+        
+        
+
+    }
+}

2b07113a0cbb3c90579693c4836f18ead466e2c8
dja <djawna@gmail.com>
2019-09-24 19:28:01 +0200

    migrate IocConfig from .net -> .net Core

---
+++ CalcTrainer.Core/IocConfig.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Linq;
+
+namespace CalcTrainer.Core
+{
+    public static class IocConfig
+    {
+
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            var contractInterfaces = Assembly.Load(nameof(CalctrainerContracts)).GetTypes().Where(i => i.IsInterface);
+
+            Debug.Assert(contractInterfaces.Any(), $"expecting interfaces in module {nameof(CalctrainerContracts)}");
+
+
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(MySQLDAL)))
+                .Where(t =>
+                {
+                    var retval = t.Namespace.Contains(nameof(MySQLDAL.repositories));
+                    return retval;
+                })
+                .As(at =>
+                {
+                    var retval2 = contractInterfaces.Single(ci => ci.Name == ("I" + at.Name));
+                    return retval2;
+                }).SingleInstance();
+
+
+            return builder.Build();
+        }
+    }
+}

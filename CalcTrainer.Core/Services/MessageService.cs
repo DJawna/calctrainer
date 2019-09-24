578228dd85dd18757bf2379b1d9722e0faf9c257
dja <djawna@gmail.com>
2019-09-24 19:32:58 +0200

    migrate Services From .net -> .net Core

---
+++ CalcTrainer.Core/Services/MessageService.cs
@@ -0,0 +1,29 @@
+﻿using Calctrainer.Core.models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calctrainer.Core.Services
+{
+    public class MessageService: INotifyPropertyChanged
+    {
+
+        private readonly ConcurrentQueue<Message> messageQueue;
+
+        public MessageService()
+        {
+            messageQueue = new ConcurrentQueue<Message>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void PushMessage(Message message)
+        {
+            messageQueue.Enqueue(message);
+        }
+    }
+}

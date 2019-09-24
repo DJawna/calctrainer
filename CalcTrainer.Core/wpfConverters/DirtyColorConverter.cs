e65ae2a10238da268a0d22fc3892e6f9390b53f5
dja <djawna@gmail.com>
2019-09-24 19:34:28 +0200

    moved converters from .net to .net core

---
+++ CalcTrainer.Core/wpfConverters/DirtyColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Calctrainer.Core.wpfConverters
+{
+    public class DirtyColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bValue = (value as bool?) ?? false;
+
+            if (bValue)
+            {
+                return Brushes.Red;
+            }
+            return Brushes.LightGray;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cValue = value as Brush ?? Brushes.LightGray;
+
+            return cValue == Brushes.Red;
+                
+            
+
+
+        }
+    }
+}
